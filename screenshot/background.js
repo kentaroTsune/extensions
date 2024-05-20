0b9d387004f278e4efed6d7666b4dcc5050d21ed
tsune <tsune@aluco.jp>
2024-05-20 12:09:48 +0900

    [Fix] テスト用console.log削除

--- screenshot/background.js
+++ screenshot/background.js
@@ -1,14 +1,11 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'capture-entire') {
-    console.log('Received capture-entire message in background.js');
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs.length > 0) {
-        console.log('Injecting capture.js into tab:', tabs[0].id);
         chrome.scripting.executeScript({
           target: { tabId: tabs[0].id },
           files: ['capture.js']
         }, () => {
-          console.log('capture.js injected, sending message to capture entire page');
           chrome.tabs.sendMessage(tabs[0].id, { message: 'capture-entire' });
         });
       } else {
@@ -16,14 +13,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       }
     });
   } else if (request.message === 'capture-visible-part') {
-    console.log('Received capture-visible-part message');
     chrome.tabs.captureVisibleTab(null, { format: 'png' }, (image) => {
-      console.log('Captured visible part');
       sendResponse({ image });
     });
-    return true; // sendResponseを非同期で使用するためにtrueを返す
+    return true;
   } else if (request.message === 'download-image') {
-    console.log('Received download-image message');
     chrome.downloads.download({
       url: request.image,
       filename: 'entire_page_screenshot.png'
